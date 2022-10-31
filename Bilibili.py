dfe6fdc44cbe145c87ca6d75edb6e4e7764d377b
SFQRM <zhuojinghan@126.com>
2022-10-31 14:16:20 +0800

    20221031

--- Bilibili.py
+++ Bilibili.py
@@ -570,4 +570,116 @@ en_result = en(x, mask)
 # print(en_result)
 # print(en_result.shape)        # （2,4,512）
 
+
 # --------------------Dncoder部分-------------------- #
+# 使用DecoderLayer的类实现解码器层
+class DecoderLayer(nn.Module):
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        """
+            初始化函数的参数有5个，分别是：
+            size：代表词嵌入的维度大小，同时也代表解码器层的尺寸，
+            self_attn：多头自注意力对象，也就是这个注意力机制需要Q=K=V，
+            src_attn：多头注意力对象，这里Q!=K=V，
+            feed_forward：前馈全连接层对象，
+            droupout：置0比率。
+        """
+        super(DecoderLayer, self).__init__()
+        # 在初始化函数中， 主要就是将这些输入传到类中
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        # 按照结构图使用clones函数克隆三个子层连接对象
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, source_mask, target_mask):
+        """
+            forward函数中的参数有4个，
+            x：来自上一层的输入，
+            mermory：来自编码器层的语义存储变量，即编码器层输出的结果，
+            source_mask：源数据掩码张量，
+            target_mask：目标数据掩码张量
+        """
+        # 将memory表示成m方便之后使用
+        m = memory
+
+        # 将x传入第一个子层结构，第一个子层结构的输入分别是x和self_attn函数，因为是自注意力机制，所以Q,K,V都是x，
+        # 最后一个参数是目标数据掩码张量，这时要对目标数据进行遮掩，因为此时模型可能还没有生成任何目标数据，
+        # 比如在解码器准备生成第一个字符或词汇时，我们其实已经传入了第一个字符以便计算损失，
+        # 但是我们不希望在生成第一个字符时模型能利用这个信息，因此我们会将其遮掩，同样生成第二个字符或词汇时，
+        # 模型只能使用第一个字符或词汇信息，第二个字符以及之后的信息都不允许被模型使用。
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, target_mask))
+
+        # 接着进入第二个子层，这个子层中是常规的注意力机制，q是输入x；k，v是编码层输出memory。
+        # 同样也传入source_mask，但是进行源数据遮掩的原因并非是抑制信息泄漏，而是遮蔽掉对结果没有意义的字符而产生的注意力值，
+        # 以此提升模型效果和训练速度。
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, source_mask))
+
+        # 最后一个子层就是前馈全连接子层
+        return self.sublayer[2](x, self.feed_forward)
+
+
+# 类的实例化参数与解码器层类似, 相比多出了src_attn, 但是和self_attn是同一个类.
+head = 8
+size = 512
+d_model = 512
+d_ff = 64
+dropout = 0.2
+self_attn = src_attn = MultiHeadedAttention(head, d_model, dropout)
+# 前馈全连接层也和之前相同
+ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+# x是来自目标数据的词嵌入表示, 但形式和源数据的词嵌入表示相同
+x = pe_result
+# memory是来自编码器的输出
+memory = en_result
+# 实际中source_mask和target_mask并不相同, 这里为了方便计算使他们都为mask
+mask = Variable(torch.zeros(8, 4, 4))
+source_mask = target_mask = mask
+
+dl = DecoderLayer(size, self_attn, src_attn, ff, dropout)
+dl_result = dl(x, memory, source_mask, target_mask)
+# print(dl_result)
+# print(dl_result.shape)          # (2,4,512)
+
+
+# 使用类Decoder来实现解码器
+class Decoder(nn.Module):
+    def __init__(self, layer, N):
+        """初始化函数的参数有两个，第一个就是解码器层layer，第二个是解码器层的个数N"""
+        super(Decoder, self).__init__()
+        # 首先使用clones方法克隆了N个layer，然后实例化了一个规范化层
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, source_mask, target_mask):
+        """forward函数中的参数有4个，x代表目标数据的嵌入表示，memory是编码器层的输出，
+           source_mask, target_mask代表源数据和目标数据的掩码张量"""
+
+        # 然后就是对每个层进行循环，当然这个循环就是变量x通过每一个层的处理，
+        # 得出最后的结果，再进行一次规范化返回即可
+        for layer in self.layers:
+            x = layer(x, memory, source_mask, target_mask)
+        return self.norm(x)
+
+
+# 分别是解码器层layer和解码器层的个数N
+size = 512
+d_model = 512
+head = 8
+d_ff = 64
+dropout = 0.2
+c = copy.deepcopy
+attn = MultiHeadedAttention(head, d_model)
+ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+layer = DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout)
+N = 8
+# 输入参数与解码器层的输入参数相同
+x = pe_result
+memory = en_result
+mask = Variable(torch.zeros(8, 4, 4))
+source_mask = target_mask = mask
+
+de = Decoder(layer, N)
+de_result = de(x, memory, source_mask, target_mask)
+print(de_result)
+print(de_result.shape)
